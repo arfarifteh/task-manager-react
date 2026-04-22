40d6d29c5db5100a330ed2c997d355aa78c1debb
Alireza Farifteh <arfarifteh@yahoo.com>
2026-04-22 15:19:13 -0400

    refactor: implement clean separation architecture for Storybook

--- src/stories/index.ts
+++ src/stories/index.ts
@@ -1,8 +1,14 @@
 // Storybook Stories Barrel Export
-// This file will be updated as stories are added
+// Clean slate - stories will be added when UI components are created
 
-// Re-export stories here for clean imports
-// export { default as ButtonStories } from './Button.stories';
-// export { default as TextFieldStories } from './TextField.stories';
-// export { default as CardStories } from './Card.stories';
-// export { default as LearningStories } from './Learning.stories';
+// Re-export UI component stories here for clean imports
+// Note: Stories import from src/components/ui/ (clean separation)
+
+// Future UI component stories (will be created with components):
+// export { default as ButtonStories } from './UI/InputComponents/Button.stories';
+// export { default as TextFieldStories } from './UI/InputComponents/TextField.stories';
+// export { default as CardStories } from './UI/DisplayComponents/Card.stories';
+// export { default as ShowcaseStories } from './UI/Showcase.stories';
+// export { default as LearningStories } from './UI/Learning.stories';
+
+// No current stories - clean slate for proper architecture
